32ace1f0f658bc43b85fad987b16e2d0bb724a67
Meeshbhoombah <rohanmishra2017@gmail.com>
2019-03-08 15:09:36 -0800

    Added reference

--- trie.go
+++ trie.go
@@ -15,74 +15,73 @@ type Trie struct {
 
 // Creates a new Trie.
 func New() {
-	h &hashNode 
-
-	var t Trie{
-		root: sha256.Hash(""),
-	}
-
-	return &t
+	return
 }
 
 // Recursively rearranges Trie to associate given key with value.
 func (t *Trie) Update(key, value string) error {
-	switch n := n.(type) {
-	case *shortNode:
-		matchlen := union(key, n.Key)
-		// Key match, update value for Short Node
-		if matchlen == len(n.Key) {
-			dirty, nn, err := t.Update(n.Val, append(prefix, key[:matchlen]...), key[matchlen:], value)
-			if !dirty || err != nil {
-				return false, n, err
-			}
-			return true, &shortNode{n.Key, nn, t.newFlag()}, nil
-		}
-		// Otherwise branch out at the index where they differ.
-		branch := &fullNode{flags: t.newFlag()}
-		var err error
-		_, branch.Children[n.Key[matchlen]], err = t.Update(nil, append(prefix, n.Key[:matchlen+1]...), n.Key[matchlen+1:], n.Val)
-		if err != nil {
-			return false, nil, err
-		}
-		_, branch.Children[key[matchlen]], err = t.Update(nil, append(prefix, key[:matchlen+1]...), key[matchlen+1:], value)
-		if err != nil {
-			return false, nil, err
-		}
 
-		// Replace node if there is no match
-		if matchlen == 0 {
-			return true, branch, nil
-		}
-		// Otherwise, replace it with a short node leading up to the branch.
-		return true, &shortNode{key[:matchlen], branch, t.newFlag()}, nil
+	/*
+	   	// go-ethereum Trie implementation
+	   	switch n := n.(type) {
+	   	case *shortNode:
+	   		matchlen := union(key, n.Key)
+	   		// Key match, update value for Short Node
+	   		if matchlen == len(n.Key) {
+	   			dirty, nn, err := t.Update(n.Val, append(prefix, key[:matchlen]...), key[matchlen:], value)
+	   			if !dirty || err != nil {
+	   				return false, n, err
+	   			}
+	   			return true, &shortNode{n.Key, nn, t.newFlag()}, nil
+	   		}
+	   		// Otherwise branch out at the index where they differ.
+	   		branch := &fullNode{flags: t.newFlag()}
+	   		var err error
+	   		_, branch.Children[n.Key[matchlen]], err = t.Update(nil, append(prefix, n.Key[:matchlen+1]...), n.Key[matchlen+1:], n.Val)
+	   		if err != nil {
+	   			return false, nil, err
+	   		}
+	   		_, branch.Children[key[matchlen]], err = t.Update(nil, append(prefix, key[:matchlen+1]...), key[matchlen+1:], value)
+	   		if err != nil {
+	   			return false, nil, err
+	   		}
 
-	case *fullNode:
-		dirty, nn, err := t.Update(n.Children[key[0]], append(prefix, key[0]), key[1:], value)
-		if !dirty || err != nil {
-			return false, n, err
-		}
-		n = n.copy()
-		n.flags = t.newFlag()
-		n.Children[key[0]] = nn
-		return true, n, nil
+	   		// Replace node if there is no match
+	   		if matchlen == 0 {
+	   			return true, branch, nil
+	   		}
+	   		// Otherwise, replace it with a short node leading up to the branch.
+	   		return true, &shortNode{key[:matchlen], branch, t.newFlag()}, nil
 
-	case nil:
-		return true, &shortNode{key, value, t.newFlag()}, nil
+	   	case *fullNode:
+	   		dirty, nn, err := t.Update(n.Children[key[0]], append(prefix, key[0]), key[1:], value)
+	   		if !dirty || err != nil {
+	   			return false, n, err
+	   		}
+	   		n = n.copy()
+	   		n.flags = t.newFlag()
+	   		n.Children[key[0]] = nn
+	   		return true, n, nil
 
-	case hashNode:
-		rn, err := sha256.New(n, prefix)
-		if err != nil {
-			return false, nil, err
-		}
-		dirty, nn, err := t.Update(rn, prefix, key, value)
-		if !dirty || err != nil {
-			return false, rn, err
-		}
-		return true, nn, nil
+	   	case nil:
+	   		return true, &shortNode{key, value, t.newFlag()}, nil
 
-	default:
-		panic(fmt.Sprintf("%T: invalid node: %v", n, n))
-	}
+	   	case hashNode:
+	   		rn, err := sha256.New(n, prefix)
+	   		if err != nil {
+	   			return false, nil, err
+	   		}
+	   		dirty, nn, err := t.Update(rn, prefix, key, value)
+	   		if !dirty || err != nil {
+	   			return false, rn, err
+	   		}
+	   		return true, nn, nil
+
+	   	default:
+	   		panic(fmt.Sprintf("%T: invalid node: %v", n, n))
+	   	}
+	   }
+	*/
 }
 
 // Returns the value stored in Trie for the given key.
