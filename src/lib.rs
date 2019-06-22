0f527227e537ac1e80aeaab4dab20f22ca43dbb4
Meeshbhoombah <rohanmishra2017@gmail.com>
2019-06-22 13:20:39 -0700

    Moved boilerplate to example, build simple contract structure in lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,51 +1,24 @@
-#![cfg_attr(not(any(test, feature = "std")), no_std)]
+#![no_std]
 
-use ink_core::{
-    env::println,
-    memory::format,
-    storage,
-};
-use ink_lang::contract;
+// Import to interact with contract storage
+use ink_core::storage;
+// Import the `contract!` macro
+use ink_core::contract;
 
+// The code for your contract will live entirely in the `contract!` macro
 contract! {
-    /// This simple dummy contract has a `bool` value that can
-    /// alter between `true` and `false` using the `flip` message.
-    /// Users can retrieve its current state using the `get` message.
-    struct Mpt {
-        /// The current state of our flag.
-        value: storage::Value<bool>,
+    struct ContractName {
+        // Contract Storage 
     }
 
-    impl Deploy for Mpt {
-        /// Initializes our state to `false` upon deploying our smart contract.
+    impl Deploy for ContractName {
         fn deploy(&mut self) {
-            self.value.set(false)
+            // Deployment logic that runs once upon contract creation 
         }
     }
 
-    impl Mpt {
-        /// Flips the current state of our smart contract.
-        pub(external) fn flip(&mut self) {
-            *self.value = !*self.value;
-        }
-
-        /// Returns the current state.
-        pub(external) fn get(&self) -> bool {
-            println(&format!("Storage Value: {:?}", *self.value));
-            *self.value
-        }
+    impl ContractName {
+        // Public/Private Function Definitions 
     }
 }
 
-#[cfg(all(test, feature = "test-env"))]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let mut contract = Mpt::deploy_mock();
-        assert_eq!(contract.get(), false);
-        contract.flip();
-        assert_eq!(contract.get(), true);
-    }
-}
