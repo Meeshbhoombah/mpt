d7cbedd25409506a15394c286b884df34b24c53f
Meeshbhoombah <rohanmishra2017@gmail.com>
2019-06-30 14:22:04 -0700

    Cannot instantiate Node, switching to Core API for more accessibility

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,34 @@
 #![cfg_attr(not(any(test, feature = "std")), no_std)]
 
+use ink_core::{
+    storage,
+};
+use ink_lang::contract;
 
+contract! {
+    struct Mpt {
+        owner: storage::Value<AccountId>,
+        node: storage::Vec<i32>,
+        nodes: storage::HashMap<Hash, self.node>,
+    }
+
+    impl Deploy for Mpt {
+        fn deploy(&mut self) {
+            self.owner.set(env.caller());
+        }
+    }
+
+    /// Public functions
+    impl Mpt {
+    }
+}
+
+#[cfg(all(test, feature = "test-env"))]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_deploys() {
+        let mut trie = Mpt::deploy_mock();
+    }
+}
