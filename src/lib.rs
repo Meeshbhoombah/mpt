5875cfe3e0946188a31284781bf3f42699dee2ab
Meeshbhoombah <rohanmishra2017@gmail.com>
2019-07-02 10:37:14 -0700

    Hasher error

--- src/lib.rs
+++ src/lib.rs
@@ -25,7 +25,49 @@ use ink_core::{
 };
 use ink_lang::contract;
 
+#[derive(Debug, Encode, Decode)]
+pub struct Node {
+    /// collection of children elements
+    ///
+    /// branch: contains one element
+    /// extension: slots for all values - a-z,0-9
+    /// leaf: None
+    children: storage::Vec<Hash>,
+    /// varying key type, assembles key for value 
+    ///
+    /// branch: prefixstring
+    /// extension: shared string
+    /// leaf: contains the value
+    key: storage::Value<String>,
+    /// varying value type
+    ///
+    /// branch: None 
+    /// extension: None 
+    /// leaf: contains the value
+    value: storage::Value<String>,
+}
 
+impl Node {
+    pub fn hash<H: EnvTypes>(&self, state: &mut H) {
+    }
+}
+
+contract! {
+    struct Mpt {
+        owner: storage::Value<AccountId>,
+        root: storage::Value<Hash>,
+        nodes: storage::HashMap<Hash, Node>,
+    }
+
+    impl Deploy for Mpt {
+        fn deploy(&mut self) {
+            self.owner.set(env.caller());
+        }
+    }
+
+    impl Mpt { 
+    }
+}
 
 #[cfg(all(test, feature = "test-env"))]
 mod tests {
@@ -33,9 +75,10 @@ mod tests {
     use ink_core::env;
 
     #[test]
-    fn it_deploys() {
+    fn it_deploys_owned() {
         // no events prior to deploy
         assert_eq!(env::test::emitted_events().count(), 0);
         let mut trie = Mpt::deploy_mock();
+        assert_eq!(trie.owner, env::TestEnvData.caller);
     }
 }
