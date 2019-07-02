dd3dd131e96a524abd66471e4b4ca50621707ab5
Meeshbhoombah <rohanmishra2017@gmail.com>
2019-07-01 22:02:21 -0700

    Starting deploy tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,41 @@
 #![cfg_attr(not(any(test, feature = "std")), no_std)]
 
+use parity_codec::{
+    Decode,
+    Encode
+};
 use ink_core::{
-    storage,
+    env::{
+        Env,
+        EnvTypes,
+        Hash,
+        AccountId
+    },
+    memory::string::String,
+    storage::{
+        self,
+        alloc::{
+            AllocateUsing,
+            BumpAlloc,
+            Initialize,
+        },
+        Flush,
+        Key
+    },
 };
 use ink_lang::contract;
 
-contract! {
-    struct Mpt {
-        owner: storage::Value<AccountId>,
-        node: storage::Vec<i32>,
-        nodes: storage::HashMap<Hash, self.node>,
-    }
 
-    impl Deploy for Mpt {
-        fn deploy(&mut self) {
-            self.owner.set(env.caller());
-        }
-    }
-
-    /// Public functions
-    impl Mpt {
-    }
-}
 
 #[cfg(all(test, feature = "test-env"))]
 mod tests {
-    use super::*;
+    use super::Mpt;
+    use ink_core::env;
 
     #[test]
     fn it_deploys() {
+        // no events prior to deploy
+        assert_eq!(env::test::emitted_events().count(), 0);
         let mut trie = Mpt::deploy_mock();
     }
 }
